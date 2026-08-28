c984866a5e50b21a904c9da7ef953c149c911a73
Sornchai Udomthanapong <sornchai.ud@go.buu.ac.th>
2026-08-28 13:29:54 +0700

    Green Screen

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,28 @@ import 'app_screen/my_statefull_widget.dart';
 
 void main() {
   runApp(
-    MyStatefulWidget(title:"StatefulWidget Example")
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+        title: 'First Flutter App',
+        home: Scaffold(
+            appBar: AppBar(
+                title: Text('Welcome to Flutter'),
+                backgroundColor: Colors.green
+            ),
+            body: Material(
+                color: Colors.lightGreen,
+                child: Center(
+                    child: Text(
+                        'Hello World',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20.0
+                        )
+                    )
+                )
+            )
+        )
+    )
   );
 }
 
